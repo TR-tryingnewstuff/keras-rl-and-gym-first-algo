bd73baf0b01b3fa3df171b8487f6ff83c84a9268
TR-tryingnewstuff <129596360+TR-tryingnewstuff@users.noreply.github.com>
2023-04-05 15:04:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,21 @@ import rl.policy
 import rl.memory
 import matplotlib.pyplot as plt
 
+WINDOW = 20
+
 # ---------------------------------- Collecting S&P500 futures contract historical data ---------------------------------------------------
 
 plot_df = pd.DataFrame(TvDatafeed().get_hist('ES1!', 'CME_MINI', Interval.in_daily, 5000))
 df = pd.DataFrame(TvDatafeed().get_hist('ES1!', 'CME_MINI', Interval.in_daily, 5000))
 
-df['up_close'] = np.where(df['close'] > df['open'], 1, -1)
-df['body'] = round(abs(df['open'] - df['close'])/ df['open']  * 100, 2)
-df['up_wick'] =  round((df['high'] - df['open'])/ df['open']  * 100, 2)
-df['down_wick'] =  round((df['open'] - df['low'])/ df['open']  * 100, 2)
-df['daily_move'] = df['up_close']*df['body']
 
-df = df.drop(['symbol', 'volume', 'open', 'high', 'low', 'close', 'up_close'], axis = 1)
+df['relative_range_position'] = (df['close'] - ((df['low'].rolling(WINDOW).min() + df['high'].rolling(WINDOW).max()) /2)) / ((df['high'].rolling(WINDOW).max() - df['low'].rolling(WINDOW).min()) /2)
+df['body'] = (df['open'] - df['close'])/ df['open']  * 100
 
-print(df.head())
+df = df.drop(['symbol', 'volume'], axis = 1).dropna()
+
+print(df.head(20))
+n_columns = len(df.columns)
 
 
 # ----------------------------- Defining the Market environment with the help of the gym.Env class -----------------------------------
@@ -36,12 +37,14 @@ class Market(gym.Env):
         # Initializing the attributes
         
         self.action_space = Discrete(3)
+        self.commission = 0
        
         self.done = False 
         self.n_step = 0
-        self.index = np.random.randint(1, 2000)
+        self.index = np.random.randint(1, 4000)
         
-        self.observation = np.asarray(df.iloc[self.index+10:self.index+20]).flatten()
+        self.observation = np.asarray(df.iloc[self.index])
+        print(self.observation.shape)
     
     
     def reset(self):
@@ -60,21 +63,21 @@ class Market(gym.Env):
         self.action = action
         self.index += 1
         self.n_step += 1
-        self.observation = np.asarray(df.iloc[self.index+10:self.index+20]).flatten()
+        self.observation = np.asarray(df.iloc[self.index])
         
         # Get reward
         
         if (self.action == 1):
-            reward = np.where(abs(self.observation[-1]) < 0.7, 0.1, -0.1)
+            reward = 0
         
         elif (self.action == 0):
-            reward = self.observation[-1] * -1 
+            reward = self.observation[-1] * -1 - self.commission
 
         else:
-            reward = self.observation[-1]
+            reward = self.observation[-1] - self.commission
         
         
-        if self.n_step == 100: 
+        if self.n_step == 500: 
             self.done = True
             
         info = {}
@@ -83,19 +86,19 @@ class Market(gym.Env):
         
         print(f'  action taken : {self.action}')
         i = self.index
-        color_index = np.where(plot_df.open[i+10:i+20] < plot_df.close[i+10:i+20], 'gray', 'black')
-        date_index = np.array(plot_df[i+10:i+20].index)
+        color_index = np.where(plot_df.open[i:i+WINDOW] < plot_df.close[i:i+WINDOW], 'gray', 'black')
+        date_index = np.array(plot_df[i:i+WINDOW].index)
         
-        bars = np.array(plot_df.close[i+10:i+20])-np.array(plot_df.open[i+10:i+20])
-        wicks = np.array(plot_df.high[i+10:i+20])-np.array(plot_df.low[i+10:i+20])
-        plt.bar(date_index, bars, width=0.7, bottom=plot_df.open[i+10:i+20], color=color_index)
-        plt.bar(date_index, wicks, width=0.1, bottom=plot_df.low[i+10:i+20], color=color_index)
+        bars = np.array(plot_df.close[i:i+WINDOW])-np.array(plot_df.open[i:i+WINDOW])
+        wicks = np.array(plot_df.high[i:i+WINDOW])-np.array(plot_df.low[i:i+WINDOW])
+        plt.bar(date_index, bars, width=0.7, bottom=plot_df.open[i:i+WINDOW], color=color_index)
+        plt.bar(date_index, wicks, width=0.1, bottom=plot_df.low[i:i+WINDOW], color=color_index)
         
         if self.action == 2:
-            plt.arrow(x=date_index[-1], y=plot_df.high[i+19]+7, dx=0, dy=-4, width=0.2, color='green')
+            plt.arrow(x=date_index[-1], y=plot_df.high[i+WINDOW-1]+7, dx=0, dy=-4, width=0.2, color='green')
             
         elif self.action == 0:
-            plt.arrow(x=date_index[-1], y=plot_df.high[i+19]+7, dx=0, dy=-4, width=0.2, color='red')
+            plt.arrow(x=date_index[-1], y=plot_df.high[i+WINDOW-1]+7, dx=0, dy=-4, width=0.2, color='red')
             
         plt.pause(0.1)
         plt.clf()
@@ -121,8 +124,8 @@ def build_model(actions):
       
     model = tf.keras.models.Sequential()
     
-    model.add(tf.keras.layers.LSTM(input_shape=(1, 40), units=40, return_sequences=True))
-    model.add(tf.keras.layers.LSTM(units=40))
+    model.add(tf.keras.layers.LSTM(input_shape=(WINDOW, n_columns), units=WINDOW*n_columns, return_sequences=True))
+    model.add(tf.keras.layers.LSTM(units=WINDOW*n_columns))
     model.add(tf.keras.layers.Flatten()) 
     model.add(tf.keras.layers.Dense(actions, activation='linear'))
 
@@ -137,9 +140,9 @@ def build_agent(model, actions):
     """Builds the agent that will interact with the environment"""
 
     policy = rl.policy.BoltzmannQPolicy()
-    memory = rl.memory.SequentialMemory(limit=10000, window_length=1)
+    memory = rl.memory.SequentialMemory(limit=10000, window_length=WINDOW)
     dqn = rl.agents.DQNAgent(model=model, memory=memory, policy=policy, nb_actions=actions, nb_steps_warmup=50)
-    
+   
     return dqn
     
 dqn = build_agent(build_model(actions), actions)
@@ -148,6 +151,8 @@ dqn = build_agent(build_model(actions), actions)
 
 dqn.compile(optimizer='RMSprop')
 dqn.fit(env, nb_steps=1000, visualize=True, verbose=1)
+
+
 
 
 
